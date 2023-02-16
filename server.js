bca780d036da0a829c568af417f493ea64c57cc2
nkarl <21993921+nkarl@users.noreply.github.com>
2023-02-15 22:16:15 -0800

    Updated.

--- server.js
+++ server.js
@@ -8,29 +8,29 @@ const HOST = "localhost";
 
 const api = express();
 const client = new Client({
-  host: "localhost",
-  database: "business_db",
-  user: "postgres",
-  password: "pass",
-  port: 5432,
+    host: "localhost",
+    database: "business_db",
+    user: "postgres",
+    password: "pass",
+    port: 5432,
 });
 
 const selectAll = async (req, res) => {
-  try {
-    await client.connect();
-    await client.query("BEGIN");
-    let statement = "SELECT * FROM business WHERE (city='Ahwatukee');";
-    const result = await client.query(statement);
-    console.log(result);
-    res.send(result.rows);
-    await client.query("ROLLBACK");
-    //await client.query("COMMIT");
-  } catch (err) {
-    await client.query("ROLLBACK");
-    console.log(res, err);
-  } finally {
-    client.end(); // pool.release();
-  }
+    try {
+        await client.connect();
+        await client.query("BEGIN");
+        let statement = "SELECT * FROM business WHERE (city='Ahwatukee');";
+        const result = await client.query(statement);
+        console.log(result);
+        res.send(result.rows);
+        await client.query("ROLLBACK");
+        //await client.query("COMMIT");
+    } catch (err) {
+        await client.query("ROLLBACK");
+        console.log(res, err);
+    } finally {
+        client.end(); // pool.release();
+    }
 };
 
 api.get("/", selectAll);
