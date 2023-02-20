f4300058e997c53758dd397ffbfb65f27172219d
nkarl <21993921+nkarl@users.noreply.github.com>
2023-02-20 07:35:23 -0800

    Working on the api endpoints.

--- server.js
+++ server.js
@@ -33,7 +33,57 @@ const selectAll = async (req, res) => {
     }
 };
 
+const selectStates = async (req, res) => {
+    try {
+        await database.connect();
+        await database.query("BEGIN");
+        let statement = "SELECT DISTINCT state FROM business ORDER BY state;";
+        const result = await database.query(statement);
+        console.log(result);
+        res.send(result.rows);
+        await database.query("ROLLBACK");
+    } catch (err) {
+        await database.query("ROLLBACK");
+    } finally {
+        database.end();
+    }
+}
+
+/*
+ * SELECT DISTINCT city
+ * FROM business
+ * WHERE state= <selected state>
+ * ORDER BY city;
+ */
+const selectCities = async (req, res) => {
+    try {
+        await database.connect();
+        await database.query("BEGIN");
+        let statement = "SELECT DISTINCT state FROM business ORDER BY state;";
+        const states = database.query(statement);
+        const getState = url => url.substring(url.lastIndexOf('/') + 1);
+        const state = getState(req.url);
+        let qresult;
+        if (states.include(state)) {
+            let statement = "SELECT DISTINCT city FROM business WHERE state=$1 ORDER BY city;";
+            qresult = await database.query(statement, [state]);
+            console.log(qresult);
+        } else {
+            qresult = req.url;
+            console.log(req.url);
+        }
+        const result = qresult;
+        res.send(result);
+        await database.query("ROLLBACK");
+    } catch (err) {
+        await database.query("ROLLBACK");
+    } finally {
+        database.end();
+    }
+}
 api.get("/", selectAll);
+api.get("/states", selectStates);
+//api.get("/states/NV", selectCities);
 
 api.listen(PORT, HOST);
 console.log(`Running on http://${PORT}:${PORT}`);
